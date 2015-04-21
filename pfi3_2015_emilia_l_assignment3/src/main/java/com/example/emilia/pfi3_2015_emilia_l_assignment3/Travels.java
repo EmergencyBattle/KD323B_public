0379ae2d286bb99e88f808a5baf27bdfe96e1e32
EmergencyBattle <emergencybattle@gmail.com>
2015-04-21 13:14:49 +0200

    sixth commit

--- pfi3_2015_emilia_l_assignment3/src/main/java/com/example/emilia/pfi3_2015_emilia_l_assignment3/Travels.java
+++ pfi3_2015_emilia_l_assignment3/src/main/java/com/example/emilia/pfi3_2015_emilia_l_assignment3/Travels.java
@@ -3,28 +3,34 @@ package com.example.emilia.pfi3_2015_emilia_l_assignment3;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ExpandableListView;
 import android.os.AsyncTask;
 import android.view.MenuItem;
-import android.widget.ExpandableListView;
+import android.widget.Spinner;
 
+import com.example.emilia.pfi3_2015_emilia_l_assignment3.skaneAPI.control.Constants;
 import com.example.emilia.pfi3_2015_emilia_l_assignment3.skaneAPI.model.Journey;
 import com.example.emilia.pfi3_2015_emilia_l_assignment3.skaneAPI.model.Journeys;
 import com.example.emilia.pfi3_2015_emilia_l_assignment3.skaneAPI.xmlparser.Parser;
 
 import java.util.ArrayList;
 
-import static com.example.emilia.pfi3_2015_emilia_l_assignment3.R.id.*;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Travels extends Fragment {
-
+public class Travels extends Fragment implements AdapterView.OnItemSelectedListener {
 
     ArrayList<Journey> journeyList = new ArrayList<Journey>();
+
+    private Spinner spinnerFrom;
+    private Spinner spinnerTo;
+    private Adapter adapter;
 
     public Travels() {
         // Required empty public constructor
@@ -35,48 +41,80 @@ public class Travels extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_travels, container,false);
+        View view = inflater.inflate(R.layout.fragment_travels, container, false);
 
-        ExpandableListView expl = (ExpandableListView)view.findViewById(expandableListView);
-        expl.setAdapter(new Adapter(getActivity(), journeyList));
+        spinnerFrom = (Spinner) view.findViewById(R.id.spinner);
+        spinnerTo = (Spinner) view.findViewById(R.id.spinner2);
+
+        ExpandableListView expl = (ExpandableListView) view.findViewById(R.id.expandableListView);
+        adapter = new Adapter(getActivity(),journeyList);
+        expl.setAdapter(adapter);
+
+        spinnerFrom.setOnItemSelectedListener(this);
+        spinnerTo.setOnItemSelectedListener(this);
 
         return view;
+
     }
 
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
-        if (id == R.id.action_search){
+        if (id == R.id.action_search) {
+            Log.i("action_search", "started");
+
+            int fromStation = spinnerFrom.getSelectedItemPosition();
+            int toStation = spinnerTo.getSelectedItemPosition();
+
+
+            String[] stationNo = getResources().getStringArray(R.array.station);
+            String searchURL = Constants.getURL(stationNo[fromStation], stationNo[toStation], 14);
+            journeyList.clear();
+
+            new DoInBackground().execute(searchURL);
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void finishedSearch (){
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+        int fromStation = spinnerFrom.getSelectedItemPosition();
+        int toStation = spinnerTo.getSelectedItemPosition();
+
+        String[] stationNo = getResources().getStringArray(R.array.station);
+        String searchURL = Constants.getURL( stationNo[fromStation], stationNo[toStation], 14);
+        new DoInBackground().execute(searchURL);
 
     }
 
-    private class DoInBackground extends AsyncTask<String,Void,Long>{
-        @Override
-        protected Long DoInBackground(String... params){
-            Journeys journeys = Parser.getJourneys(params[0]);
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    private class DoInBackground extends AsyncTask<String, Void, Long> {
+        @Override
+        protected Long doInBackground(String... params){
+            Journeys journeys = Parser.getJourneys(params[0]);
             journeyList.clear();
             journeyList.addAll(journeys.getJourneys());
             return null;
-
         }
 
         @Override
-        protected void onPostExecute(Long result){
-            finishedSearch();
+        protected void onPostExecute (Long result){
+
+            adapter.notifyDataSetChanged();
+            for (Journey si :journeyList){
+                Log.i("ExpFragment", "moment" + si.getStartStation().getStationName());
+
+            }
 
         }
-
     }
-
 }
